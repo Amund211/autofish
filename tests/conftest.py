e5494c640cc0e3e7891f012edce75044569036c8
Amund Eggen Svandal <aesvandal@gmail.com>
2021-08-24 21:14:45 +0200

    Start adding rcon setup code

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,13 @@ import shutil
 import subprocess
 import sys
 import time
+from dataclasses import dataclass
 from pathlib import Path
 
 import pytest
 import requests
+from mcrcon import MCRcon
+
 from autofish.versions import LATEST_VERSION
 
 # Args passed to java for running the minecraft server
@@ -21,11 +24,19 @@ RCON_PORT = 25575
 MINECRAFT_PORT = 25565
 
 
+@dataclass
+class Server:
+    host: str
+    port: int
+    rcon_port: int
+    rcon_password: str
+    version: str
+
+
 class InvalidVersionError(ValueError):
     pass
 
 
-# https://docs.pytest.org/en/latest/how-to/parametrize.html
 def pytest_addoption(parser):
     parser.addoption(
         "--server",
@@ -33,7 +44,7 @@ def pytest_addoption(parser):
         default=[],
         help=(
             "list of servers to test, either 'version:<versionstring>' or "
-            "'server:<ip:port:rcon_port:rcon_password>'"
+            "'server:<host:port:rcon_port:rcon_password:version>'"
         ),
     )
 
@@ -50,10 +61,11 @@ def java_args(request):
     return request.config.getoption("arg")
 
 
+# https://docs.pytest.org/en/latest/how-to/parametrize.html
 def pytest_generate_tests(metafunc):
-    if "server" in metafunc.fixturenames:
+    if "server_config" in metafunc.fixturenames:
         metafunc.parametrize(
-            "server",
+            "server_config",
             metafunc.config.getoption("server", f"version:{LATEST_VERSION}"),
             scope="session",
         )
@@ -133,8 +145,8 @@ def version_manifest(version_manifest_path, version_manifest_url):
 
 
 @pytest.fixture(scope="session")
-def minecraft_server(
-    server,
+def server(
+    server_config,
     java_args,
     version_manifest,
     minecraft_server_directory,
@@ -142,12 +154,18 @@ def minecraft_server(
     eula_path,
     tmp_path_factory,
 ):
-    """Return the (port, ip) to a running minecraft server"""
-    if server.startswith("server:"):
-        ip, port, rcon_port, rcon_password = server[7:].split(":")
-        yield ip, port, rcon_port, rcon_password
+    """Return a `Server` instance for a running minecraft server"""
+    if server_config.startswith("server:"):
+        host, port, rcon_port, rcon_password, version = server_config[7:].split(":")
+        yield Server(
+            host=host,
+            port=int(port),
+            rcon_port=int(rcon_port),
+            rcon_password=rcon_password,
+            version=version,
+        )
     else:
-        requested_version = server[8:]
+        requested_version = server_config[8:]
         server_path = minecraft_server_directory / f"{requested_version}.jar"
 
         if not server_path.is_file():
@@ -188,14 +206,40 @@ def minecraft_server(
                 *java_args,
             )
         )
-        # Wait for server to start
-        yield "localhost", MINECRAFT_PORT, RCON_PORT, RCON_PASSWORD  # Temp for testing
-        # yield ip, port
+        # TODO: Read from stdout of the server until we know it has started
+
+        yield Server(
+            host="localhost",
+            port=MINECRAFT_PORT,
+            rcon_port=RCON_PORT,
+            rcon_password=RCON_PASSWORD,
+            version=requested_version,
+        )
         # Shut down server
         pass
 
 
 @pytest.fixture()
-def working_fishing_setup(minecraft_server):
+def setup_spawn(server):
+    # Do some rcon magic to fix the setup
+    with MCRcon(server.host, server.password, server.port) as mcr:
+        # Set world spawn so that the set area is loaded
+        mcr.command("/setworldspawn 0 60 0")
+
+        mcr.command("/gamerule doMobSpawning false")
+        mcr.command("/gamerule doDaylightCycle false")
+        mcr.command("/time set 18000")
+        mcr.command("/clear @a")
+        mcr.command("/kill @e[type=!player]")
+        mcr.command("/kill @e[type=!player]")  # kill any dropped items
+
+        # Create a hallway where we can perform our tests
+        mcr.command("/fill -1 59 -1 10 62 1 minecraft:glass")
+        mcr.command("/fill 0 60 0 9 61 0 minecraft:air")
+        mcr.command("/fill 0 60 0 9 60 0 minecraft:water")
+
+
+@pytest.fixture()
+def working_fishing_setup(server):
     # Do some rcon magic to fix the setup
     pass
