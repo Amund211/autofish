e38bca18b775ef3cdfd07a852938136b2f8b19ef
Amund Eggen Svandal <aesvandal@gmail.com>
2021-08-28 00:49:39 +0200

    Rename server fixture

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,9 +1,9 @@
-def test_fish(minecraft_server, working_fishing_setup):
-    print(minecraft_server)
+def test_fish(server, working_fishing_setup):
+    print(server)
 
 
-def test_not_fish(minecraft_server, working_fishing_setup):
-    print(minecraft_server)
+def test_not_fish(server, working_fishing_setup):
+    print(server)
     import time
 
-    time.sleep(100)
+    time.sleep(10)
