0ec80fdb4ae3d091bbc418cb6c5e1acf6c033799
Amund Eggen Svandal <aesvandal@gmail.com>
2021-08-28 21:44:56 +0200

    Add a default config

--- tests/constants.py
+++ tests/constants.py
@@ -4,3 +4,22 @@ RCON_PASSWORD = "autofish-testing"
 RCON_PORT = 25575
 MINECRAFT_PORT = 25565
 FISHING_USERNAME = "testfisherman"
+
+
+DEFAULT_CONFIG = {
+    "options": {
+        "profile_path": "profile.json",
+        "print_output": True,
+        "greet_message": "TESTING GREET MESSAGE",
+        "sleep_message": "TESTING SLEEP MESSAGE",
+        "sleep_command": "sleep",
+        "sleep_time": 10,
+        "sleep_helper": True,
+        "fish_timeout": 60,
+        "durability_threshold": 30,
+        "username": FISHING_USERNAME,
+    },
+    "host": {
+        "offline": True,
+    },
+}
