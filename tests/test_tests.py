d31cf659a3349bd82f7182fea16a3637f8d1ce5f
Amund Eggen Svandal <aesvandal@gmail.com>
2021-08-28 21:45:45 +0200

    Test that login works

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,9 +1,14 @@
-def test_fish(server, working_fishing_setup):
-    print(server)
+import time
+
+from .helpers import start_client, stop_client, wait_for_login
+
+
+def test_login(server, working_fishing_setup, tmp_path):
+    client_process = start_client(tmp_path, server)
+    wait_for_login(client_process)
+    print(stop_client(client_process))
 
 
 def test_not_fish(server, working_fishing_setup):
-    print(server)
-    import time
 
-    time.sleep(10)
+    time.sleep(1)
